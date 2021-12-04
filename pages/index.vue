80e738b4d6488a95595db6383d090b906d206ac7
chrissyast <cjastles@gmail.com>
2021-12-04 23:09:12 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Person } from '../interfaces/Person'
+
+export default Vue.extend({
+  data () {
+    return {
+      people: [] as Person[]
+    }
+  },
+  computed: {
+    peopleWithPhone() : Person[] {
+      return this.people.filter(p => p.mobile)
+    }
+  }
+})
+</script>
