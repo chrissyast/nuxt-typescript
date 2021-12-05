5d3b009af425107fbad57d4d1fe8696d4ffcdb8c
chrissyast <cjastles@gmail.com>
2021-12-05 15:54:06 +0000

    add MyComponent

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     Hello
+    <MyComponent />
+    <span class="foo">Foo</span>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import MyComponent from '~/components/MyComponent.vue'
 
 export default Vue.extend({
-
+  components: { MyComponent }
 })
 </script>
