9478a00c62b7b883c81f99fbf84ebb3a171e376c
chrissyast <cjastles@gmail.com>
2021-12-05 15:17:18 +0000

    strip out "out-of-box" files

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,13 @@
 <template>
-  <Tutorial />
+  <div>
+    Hello
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { Person } from '../interfaces/Person'
 
 export default Vue.extend({
-  data () {
-    return {
-      people: [] as Person[]
-    }
-  },
-  computed: {
-    peopleWithPhone() : Person[] {
-      return this.people.filter(p => p.mobile)
-    }
-  }
+
 })
 </script>
